425573bf85c010c038dd686b080dffc7099916ce
CookieCutter4321 <65974620+CookieCutter4321@users.noreply.github.com>
2026-03-24 23:41:04 -0400

    add TODOs

--- AeroFeed.Server/Program.cs
+++ AeroFeed.Server/Program.cs
@@ -34,3 +34,16 @@ app.MapControllers();
 app.MapFallbackToFile("/index.html");
 
 app.Run();
+
+/*
+ * 
+ * TODO: We can use a bloom filter as the data does not need to be 100% accurate when considering duplication
+ * It's possible that, due to some unfortunate timing, we may end up replaying some messages when the service restarts on Container Apps.
+ * On the other hand, if we simply increment the count on redis WITH bloom bilters, the 1% false positives will evenetually compound.
+ * My hope is to eliminate this by bucketing the data on Redis and ONLY writing to the most recent time bucket with a defined TTL.
+ * Any bad data will be removed after the TTL expires.
+ * 
+ * TODO: The dockerfile certs are currently hardcoded, meaning it won't work on docker. We should do the following:
+ * 1. Running Docker locally - copy the certs (based off of KAFKA_CERT_LOCATION) to the output folder and use those.
+ * 2. Running Docker in production - use secrets and mount them to the container, then use those paths in the config.
+ */
